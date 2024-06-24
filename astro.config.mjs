8db974e589d1c1839ce5c45edddf526b46fae624
s1gr1d <sigrid.huemer@posteo.at>
2024-06-24 10:02:59 +0200

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import sentry from '@sentry/astro';
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [
-    sentry({
-      dsn: import.meta.env.PUBLIC_SENTRY_DSN,
-    }),
-  ],
+  adapter: netlify(),
+  integrations: [sentry({
+    dsn: import.meta.env.PUBLIC_SENTRY_DSN
+  })]
 });
