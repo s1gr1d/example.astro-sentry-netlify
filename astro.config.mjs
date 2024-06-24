ef5fd088ea8235f4f0561900d4d2e9f68bc6a71c
s1gr1d <sigrid.huemer@posteo.at>
2024-06-24 11:06:20 +0200

    add page without fetch call

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import sentry from '@sentry/astro';
 import netlify from '@astrojs/netlify';
 
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: netlify(),
-  integrations: [sentry({
-    dsn: import.meta.env.PUBLIC_SENTRY_DSN
-  })]
+  integrations: [
+    sentry({
+      dsn: import.meta.env.PUBLIC_SENTRY_DSN,
+    }),
+  ],
 });
